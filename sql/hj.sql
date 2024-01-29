9c8424eee44b780d76385e5c09126abb4a3cb53b
RunningHu <991287069@qq.como>
2024-01-29 15:34:13 +0800

    增加：数据集中文名称和indexName的字典表管理，以及DDL语句

--- sql/hj.sql
+++ sql/hj.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 19/12/2023 15:48:18
+ Date: 29/01/2024 15:33:14
 */
 
 SET NAMES utf8mb4;
@@ -22,35 +22,49 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `field_name`;
 CREATE TABLE `field_name`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ch_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `en_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `field_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `notes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `create_time` datetime(0) DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `ch_name`(`ch_name`) USING BTREE,
-  UNIQUE INDEX `en_name`(`en_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+                               `id` int(11) NOT NULL AUTO_INCREMENT,
+                               `ch_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `en_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `field_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `notes` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `create_time` datetime(0) DEFAULT CURRENT_TIMESTAMP,
+                               PRIMARY KEY (`id`) USING BTREE,
+                               UNIQUE INDEX `ch_name`(`ch_name`) USING BTREE,
+                               UNIQUE INDEX `en_name`(`en_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for index_name
+-- ----------------------------
+DROP TABLE IF EXISTS `index_name`;
+CREATE TABLE `index_name`  (
+                               `id` int(11) NOT NULL AUTO_INCREMENT,
+                               `index` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `show_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+                               `create_time` datetime(0) DEFAULT CURRENT_TIMESTAMP,
+                               PRIMARY KEY (`id`) USING BTREE,
+                               UNIQUE INDEX `index`(`index`) USING BTREE,
+                               UNIQUE INDEX `show_name`(`show_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for task_info
 -- ----------------------------
 DROP TABLE IF EXISTS `task_info`;
 CREATE TABLE `task_info`  (
-  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键',
-  `task_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务名称',
-  `task_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务类型',
-  `task_state` int(8) DEFAULT NULL,
-  `task_state_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务状态',
-  `split` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分隔符',
-  `is_has_title` int(8) DEFAULT NULL COMMENT '文件是否有表头',
-  `is_title_has_ch` int(8) DEFAULT NULL COMMENT '表头是否含中文',
-  `is_custom_title` int(8) DEFAULT NULL COMMENT '是否自定义表头',
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '表头',
-  `start_time` double DEFAULT NULL COMMENT '开始时间',
-  `finish_time` double DEFAULT NULL COMMENT '结束时间',
-  PRIMARY KEY (`id`) USING BTREE
+                              `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键',
+                              `task_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务名称',
+                              `task_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务类型',
+                              `task_state` int(8) DEFAULT NULL,
+                              `task_state_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务状态',
+                              `split` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分隔符',
+                              `is_has_title` int(8) DEFAULT NULL COMMENT '文件是否有表头',
+                              `is_title_has_ch` int(8) DEFAULT NULL COMMENT '表头是否含中文',
+                              `is_custom_title` int(8) DEFAULT NULL COMMENT '是否自定义表头',
+                              `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '表头',
+                              `start_time` double DEFAULT NULL COMMENT '开始时间',
+                              `finish_time` double DEFAULT NULL COMMENT '结束时间',
+                              PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
